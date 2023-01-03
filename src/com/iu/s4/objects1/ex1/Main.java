30e2f3b939b494f8a46635604514d4512aa49a44
hayoung jang <wkdgkdud1005@gmail.com>
2023-01-03 14:18:35 +0900

    test

--- src/com/iu/s4/objects1/ex1/Main.java
+++ src/com/iu/s4/objects1/ex1/Main.java
@@ -3,7 +3,7 @@ package com.iu.s4.objects1.ex1;
 public class Main {
 
 	public static void main(String[] args) {
-		
+		System.out.println("start");
 		System.out.println("메인 실행");
 		
 		MenuController mc = new MenuController();
